2cd13b0a6f331582655c2395d67a93393b065976
Flavio Andrade <flavio.ssiv@gmail.com>
2019-09-06 19:58:11 -0300

    ...

---
+++ src/utils/movieDB.js
@@ -0,0 +1,8 @@
+class MovieDB {
+  constructor() {
+    let BASE_URL = "https://api.themoviedb.org/3/";
+    let API_KEY = "api_key=3276fa51d16eb0a7c0fcb23665588bcd";
+  }
+}
+
+export default MovieDB;
