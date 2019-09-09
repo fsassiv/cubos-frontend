1fc02616e2dc8d060fd240b4b75c5c2859d6506d
Flavio Andrade <flavio.ssiv@gmail.com>
2019-09-08 22:56:45 -0300

    Updates

---
+++ precache-manifest.151b761a5c2bb503fbacd0d5f43fe0a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603959025edf9c269175479b120ac44d",
+    "url": "/cubos-frontend/index.html"
+  },
+  {
+    "revision": "184ae9b7500ec54ebf91",
+    "url": "/cubos-frontend/static/css/main.7804aa0a.chunk.css"
+  },
+  {
+    "revision": "9a8d73cda5b571c425b0",
+    "url": "/cubos-frontend/static/js/2.244eced3.chunk.js"
+  },
+  {
+    "revision": "184ae9b7500ec54ebf91",
+    "url": "/cubos-frontend/static/js/main.d31a1ffb.chunk.js"
+  },
+  {
+    "revision": "a7391d838745e3cd5c94",
+    "url": "/cubos-frontend/static/js/runtime~main.4f0c95d8.js"
+  }
+]);
